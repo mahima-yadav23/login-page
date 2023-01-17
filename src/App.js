bea23ef30ea8cf064c40b34902feef10cdb7529a
mahima-yadav23 <115358129+mahima-yadav23@users.noreply.github.com>
2023-01-17 12:29:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter , Route, Routes } from "react-router-dom"
+
+import LandingPage from './components/pages/LandingPage'
+import LoginPage from './components/pages/LoginPage'
+import RegisterPage from './components/pages/RegisterPage'
+import ForgetPasswordPage from './components/pages/ForgetPasswordPage'
+import HomePage from './components/pages/HomePage'
+
+import './App.css'
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div>
+                <Routes>
+                   
+                    <Route exact path="/" element={<LandingPage/>  } />
+                    <Route path="/login" element = {<LoginPage />  } />
+                    <Route path="/register" element={< RegisterPage /> } />
+                    <Route path="/forget-password" element={ <ForgetPasswordPage/> } />
+                    <Route path="/home" element={ <HomePage/> } />
+                </Routes>
+            
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default App
